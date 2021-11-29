9fd5b9684f14483dacb9f3972cc1a18b9f4cb751
Btaft6278 <btaft6278@gmail.com>
2021-11-28 20:37:34 -0500

    update server

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const PORT = 3000
 const twilio = require('twilio');
-const accountSid = proccess.env.ACCOUNT_SID;
-const authToken = process.env.AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const phoneNumber = process.env.TWILIO_PHONE_NUMBER;
+
 
 require('dotenv').config()
+const client = require("twilio")(accountSid, authToken);
 
 app.use(cors());
 
@@ -26,7 +28,7 @@ app.post("/send-message", async(req, res) => {
     try {
       let response = await client.messages.create({
         body: req.body.message,
-        from: process.env.TWILIO_NUMBER,
+        from: process.env.TWILIO_PHONE_NUMBER,
         to: req.body.to
       })
       res.status(200).json({
