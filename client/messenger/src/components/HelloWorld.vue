55719e70cd63828ab1e98f5f5eeec22f9c92a5c0
Btaft6278 <btaft6278@gmail.com>
2021-11-29 20:52:39 -0500

    eod push

--- client/messenger/src/components/HelloWorld.vue
+++ client/messenger/src/components/HelloWorld.vue
@@ -1,58 +1,77 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 offset-md-4">
+          <div class="card p-4">
+            <h1>Twilio App</h1>
+            <form @submit.prevent="sendMessage">
+              <div class="form-group">
+                <label for="Number">Enter Phone Number</label>
+                <input
+                  v-model="message.to"
+                  type="text"
+                  placeholder="080********"
+                  class="form-control"
+                  required
+                />
+              </div>
+              <div class="form-group">
+                <label for="Number">Enter Message</label>
+                <textarea
+                  v-model="message.message"
+                  class="form-control"
+                  id
+                  cols="30"
+                  rows="10"
+                  required
+                ></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary">
+                <span
+                  v-if="loading"
+                  class="spinner-grow spinner-grow-sm"
+                  role="status"
+                  aria-hidden="true"
+                ></span>Send
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
-
 <script>
+import swal from "sweetalert";
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      message: {
+        to: "",
+        message: ""
+      },
+      loading: false
+    };
+  },
+  methods: {
+    async sendMessage() {
+      this.loading = true;
+      try {
+        let response = await axios.post(
+          "http://localhost:3000/send-message",
+          this.message
+        );
+        console.log(response);
+        swal("Success", response.data.message, "success");
+        this.loading = false;
+      } catch (err) {
+        swal("Error", "Something Went Wrong", "error");
+        this.loading = false;
+        console.log(err);
+      }
+    }
   }
-}
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- client/messenger/src/main.js
+++ client/messenger/src/main.js
@@ -6,3 +6,12 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+import {
+  BootstrapVue,
+  IconsPlugin
+} from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
