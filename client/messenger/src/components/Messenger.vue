f519709809133c68072d112c8176e084840835fc
Btaft6278 <btaft6278@gmail.com>
2021-12-14 12:39:21 -0500

    added previews

--- client/messenger/src/components/Messenger.vue
+++ client/messenger/src/components/Messenger.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="back-container">
-    <h2 class="back-title">Send A Message</h2>
+    <h2 class="message-title">Send A Message</h2>
     <form @submit.prevent="sendMessage">
       <div class="input-container">
         <input
